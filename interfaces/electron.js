2e0ee8aeb328fbb0ce58f65afec3fd83279e8bb3
Matt Hink <matt.hink@formidable.com>
2017-03-26 14:39:20 -0700

    Woohoo! Got a window showing and everything! :)

--- interfaces/electron.js
+++ interfaces/electron.js
@@ -1,13 +1,19 @@
 declare module 'electron' {
-  declare type $SupportedAppEvent = (
-    'ready'
-  );
   declare type ElectronApp = {|
-    on: ($SupportedAppEvent, Function) => ElectronApp,
+    on: (string, Function) => ElectronApp,
+    once: (string, Function) => ElectronApp,
     removeListener: (string, Function) => ElectronApp,
   |};
 
-  declare var exports: {
-    app: ElectronApp
-  };
+  declare var app: ElectronApp;
+
+  declare class BrowserWindow {
+    on: (string, Function) => BrowserWindow;
+    once: (string, Function) => BrowserWindow;
+    removeListener: (string, Function) => BrowserWindow;
+    setParentWindow: (?BrowserWindow) => void;
+    getParentWindow: () => BrowserWindow;
+    show: () => void;
+    loadUrl: (string, ?Object) => void;
+  }
 };
--- testApp/main.js
+++ testApp/main.js
@@ -1,54 +1,17 @@
 import React from 'react';
 import Ionize from '../lib/IonizeFiber';
-import { map } from 'lodash';
 
-const handleReady = (e) => {
-  console.log("App is ready!");
-  console.log(e.sender.isReady());
-};
-
-const handleQuit = () => {
-  console.log("Goodbye!");
-};
-
-class Bar extends React.Component {
-  componentWillUnmount() {
-    console.log("Bar.componentWillUnmount()");
-  }
-
-  render() {
-    return <foo />
-  }
-}
-
-class TestApp extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      on: true,
-    };
-  }
-
-  componentDidMount() {
-    setTimeout(() => {
-      this.setState({ on: false });
-    }, 1000);
-  }
-
-  render() {
-    return <app 
-    ref={c => {this._app = c; }}
-    onReady={handleReady}>
-      <foo />
-      { this.state.on &&
-        <Bar />
-      }
-    </app>
-  }
+function handleReady() {
+  console.log("window is ready to show");
 }
 
 Ionize.start(
-  <TestApp />,
+  <app>
+    <window
+      onReadyToShow={handleReady}
+      file='index.html'
+      />
+  </app>,
   () => {
     console.log("--- Finished Ionize.start()");
   }
