0c895bb46ae28d11baabcdb1a25a07dedc22b002
Matt Hink <matt.hink@formidable.com>
2017-03-27 15:56:24 -0700

    Add position/size props to WindowElement

---
+++ testApp/demo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Ionize from 'ionize';
+
+Ionize.start(
+  <app>
+    <window
+      size={[640, 480]}
+      position={[20, 80]}
+      show
+      file="index.html" />
+  </app>
+);
